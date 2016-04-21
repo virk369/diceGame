0295a3c2a7225a0cb7df3b5f1da13cb6a48e77f0
virk369 <virk369@gmail.com>
2016-04-21 10:36:13 -0700

    internal documentaion

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-
+document.getElementById("btn").addEventListener("click", diceGame);
 
 function diceGame(){
     console.log("Game started");
     
+    // math function to generate random numbers
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
     
+    // variables get the documents by hteir ids
+    var dice1 = document.getElementById("dice1").src = "images/" + d1 + ".png";
+    var dice2 = document.getElementById("dice2").src = "images/" + d2 + ".png";
+    console.log("Game ended");
     
 };
 
